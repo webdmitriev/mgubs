267eba0206e6ab73ca5c70a271c3a16473e75bf0
webdmitriev <webdmitriev@gmail.com>
2025-11-27 20:46:39 +0300

    block-06 is ready

--- pages/page-gutenberg.php
+++ pages/page-gutenberg.php
@@ -8,7 +8,19 @@ get_header();
 the_content();
 ?>
 
+<div class="block-standard block-06">
+  <div class="container">
+    <h3 class="h3">Не увидели подходящую программу?</h3>
+    <p class="descr">Свяжитесь с нашим менеджером, чтобы получить дополнительную информацию по программам</p>
+    <button class="btn btn-orange">Свяжитесь с нами</button>
+  </div>
+</div>
 
+<div class="block-standard block-07">
+  <div class="container">
+    
+  </div>
+</div>
 
 <?php
 get_footer();
