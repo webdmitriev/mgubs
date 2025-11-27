d099b063a07df89fa3539a1a1fbbccbc5a168de3
webdmitriev <webdmitriev@gmail.com>
2025-11-27 21:54:08 +0300

    block-08 is ready

--- pages/page-gutenberg.php
+++ pages/page-gutenberg.php
@@ -49,5 +49,35 @@ the_content();
   </div>
 </div>
 
+<div class="block-standard block-08">
+  <div class="container">
+    <div class="teacher-article">
+      <img src="https://mgubs.ru/wp-content/uploads/2022/12/oleg_vichansky_500.jpg" alt="alto" class="teacher-image" />
+      <div class="teacher-title">Виханский Олег Самуилович</div>
+      <div class="teacher-descr">Декан Высшей школы бизнеса Доктор экономических наук Заслуженный профессор МГУ</div>
+      <div class="teacher-link">Подробнее</div>
+    </div>
+    <div class="teacher-article">
+      <img src="https://mgubs.ru/wp-content/uploads/2022/12/oleg_vichansky_500.jpg" alt="alto" class="teacher-image" />
+      <div class="teacher-title">Виханский Олег Самуилович</div>
+      <div class="teacher-descr">Декан Высшей школы бизнеса Доктор экономических наук Заслуженный профессор экономических наук Заслуженный профессор МГУ</div>
+      <div class="teacher-link">Подробнее</div>
+    </div>
+    <div class="teacher-article">
+      <img src="https://mgubs.ru/wp-content/uploads/2022/12/oleg_vichansky_500.jpg" alt="alto" class="teacher-image" />
+      <div class="teacher-title">Виханский Олег Самуилович</div>
+      <div class="teacher-descr">Декан Высшей школы бизнеса Доктор экономических наук Заслуженный профессор МГУ</div>
+      <div class="teacher-link">Подробнее</div>
+    </div>
+    <div class="teacher-article">
+      <img src="https://mgubs.ru/wp-content/uploads/2022/12/oleg_vichansky_500.jpg" alt="alto" class="teacher-image" />
+      <div class="teacher-title">Виханский Олег Самуилович</div>
+      <div class="teacher-descr">Декан Высшей школы бизнеса Доктор экономических наук Заслуженный профессор экономических наук Заслуженный профессор МГУ</div>
+      <div class="teacher-link">Подробнее</div>
+    </div>
+  </div>
+  <a href="//192.168.1.86:3000/mgubs/" class="btn btn-white">Все новости</a>
+</div>
+
 <?php
 get_footer();
