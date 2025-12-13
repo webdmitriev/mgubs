70dd80cee47f1dd487fed9e1b755e8a3cb1327dd
webdmitriev <webdmitriev@gmail.com>
2025-12-14 00:53:02 +0300

    block-17 is changed (AutoSlide to SlickSlider)

--- assets/css-js/app.js
+++ assets/css-js/app.js
@@ -3348,6 +3348,31 @@ document.addEventListener("DOMContentLoaded", () => {
 /* slider.js */
 document.addEventListener('DOMContentLoaded', function () {
 
+  $('.block-17 .slider-items').each(function () {
+    const slider = $(this);
+    slider.slick({
+      autoplay: true,
+      autoplaySpeed: 7000,
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      centerMode: true,
+      prevArrow: '<button class="slick-arrow slick-arrow-prev"></button>',
+      nextArrow: '<button class="slick-arrow slick-arrow-next"></button>',
+      rows: 0
+    });
+
+    const sLightbox = $(this);
+    sLightbox.slickLightbox({
+      src: 'src',
+      itemSelector: '.slider-item',
+      navigateByKeyboard: true
+    });
+    console.log(sLightbox);
+  })
+
   $('.block-19 .content-reviews').each(function () {
     const slider = $(this);
     slider.slick({
--- pages/page-gutenberg.php
+++ pages/page-gutenberg.php
@@ -11,13 +11,13 @@ the_content();
 
 <div class="block-standard block-17">
   <!-- owner-slider-with-dots owner-slider-with-arrows -->
-  <div class="slider-items block-17-slider-items-js owner-slider">
-    <img src="<?= $url; ?>/assets/img/slider/slide-01.jpg" alt="alto" class="slider-item owner-slide slide show-popup-gallery" />
-    <img src="<?= $url; ?>/assets/img/slider/slide-02.jpg" alt="alto" class="slider-item owner-slide slide show-popup-gallery" />
-    <img src="<?= $url; ?>/assets/img/slider/slide-03.jpg" alt="alto" class="slider-item owner-slide slide show-popup-gallery" />
-    <img src="<?= $url; ?>/assets/img/slider/slide-04.jpg" alt="alto" class="slider-item owner-slide slide show-popup-gallery" />
-    <img src="<?= $url; ?>/assets/img/slider/slide-05.jpg" alt="alto" class="slider-item owner-slide slide show-popup-gallery" />
-    <img src="<?= $url; ?>/assets/img/slider/slide-06.jpg" alt="alto" class="slider-item owner-slide slide show-popup-gallery" />
+  <div class="slider-items">
+    <img src="<?= $url; ?>/assets/img/slider/slide-01.jpg" alt="alto" class="slider-item" />
+    <img src="<?= $url; ?>/assets/img/slider/slide-02.jpg" alt="alto" class="slider-item" />
+    <img src="<?= $url; ?>/assets/img/slider/slide-03.jpg" alt="alto" class="slider-item" />
+    <img src="<?= $url; ?>/assets/img/slider/slide-04.jpg" alt="alto" class="slider-item" />
+    <img src="<?= $url; ?>/assets/img/slider/slide-05.jpg" alt="alto" class="slider-item" />
+    <img src="<?= $url; ?>/assets/img/slider/slide-06.jpg" alt="alto" class="slider-item" />
   </div>
 </div>
 
