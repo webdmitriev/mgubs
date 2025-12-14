44a7e25c06fd1d1612e8233695e328a8d92b8783
webdmitriev <webdmitriev@gmail.com>
2025-12-14 16:15:47 +0300

    block-07 is done

--- development/gutenberg/blocks/block-07/attributes.js
+++ development/gutenberg/blocks/block-07/attributes.js
@@ -1,6 +1,7 @@
 export default {
   anchor: { type: 'string', default: '' },
   bgc: { type: 'string', default: '' },
+
   title: { type: 'string', default: '' },
   descr: { type: 'string', default: '' },
   buttonText: { type: 'string', default: '' },
