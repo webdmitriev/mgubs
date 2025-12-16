497b84efebc2957c5c72ea6f3df54e9a67424559
webdmitriev <webdmitriev@gmail.com>
2025-12-16 20:11:27 +0300

    block-24 is ready

--- pages/page-development.php
+++ pages/page-development.php
@@ -8,81 +8,31 @@ get_header();
 
 $url = get_template_directory_uri();
 $image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAAAAHAAUAAAIFjI+puwUAOw==';
+the_content();
 ?>
 
-<main class="mgu-main">
-  <img class="mgu-main__bg" src="<?= $url; ?>/assets/img/mgu-main/mgu-main-1920.jpg" alt="alto" />
-  <div class="container df-sp-ce">
-    <div class="mgu-main__content">
-      <h1 class="h1">Бакалавриат ВШБ МГУ</h1>
-      <p class="sub_title">Управление бизнесом и предпринимательство</p>
-      <div class="divider"></div>
-      <p class="sub_title">4-х годичная программа управленческого образования</p>
-      <p class="descr">Направлена на получение студентами общих знаний по бизнесу, управлению, экономике, другим социально-экономическим дисциплинам, освоение навыков управленческой деятельности, выработке у студентов передового взгляда на управленческий процесс и формирование основ поведения, позволяющего добиться успеха в деловых организациях</p>
-    </div>
-    <div class="wpcf7">
-      <h3 class="form-title">Запросить презентацию</h3>
-      <div class="form-group form-group-input">
-        <span class="form-group_label">Имя</span>
-        <input class="form-group_input" type="text" placeholder="Имя" />
-        <span class="wpcf7-not-valid-tip">Важно заполнить это поле.</span>
-      </div>
-      <div class="form-group form-group-input">
-        <span class="form-group_label">Фамилия</span>
-        <input class="form-group_input" type="text" placeholder="Фамилия" />
-        <span class="wpcf7-not-valid-tip">Важно заполнить это поле.</span>
-      </div>
-      <div class="form-group form-group-input">
-        <span class="form-group_label">E-mail</span>
-        <input class="form-group_input" type="email" placeholder="E-mail" />
-        <span class="wpcf7-not-valid-tip">Важно заполнить это поле.</span>
-      </div>
-      <div class="form-group form-group-input">
-        <span class="form-group_label">Контактный номер</span>
-        <input class="form-group_input" type="text" placeholder="Контактный номер" />
-        <span class="wpcf7-not-valid-tip">Важно заполнить это поле.</span>
-      </div>
-      <div class="form-group form-group-checkbox">
-        <input type="checkbox" />
-        <div class="checkbox"></div>
-        <span>Даю разрешение <br/>на обработку данных</span>
-      </div>
-      <div class="form-group form-group-submit">
-        <input type="submit" value="Отправить" />
-      </div>
-    </div>
-  </div>
-</main>
+<div class="block-standard block-24">
+  <picture>
+    <source srcset="https://mgubs.ru/wp-content/uploads/2023/07/ma-bg-576x.jpg.webp" type="image/webp" media="(max-width: 576px)">
+    <source srcset="https://mgubs.ru/wp-content/uploads/2023/07/ma-bg-576x.jpg" type="image/jpeg" media="(max-width: 576px)">
 
-<section class="mgu-advantages">
-  <div class="container">
-    <div class="block-title"><h2 class="h2">Title</h2></div>
-    <div class="advantages-item">
-      <img class="advantages-item__icon" src="<?= $url; ?>/assets/img/mgu-advantages/icon-1.svg" alt="alto" />
-      <div class="advantages-item__content"><strong>Learning by doing</strong> <br/>Образовательный процесс построен на использовании активных методов обучения. Данная методика позволяет студентам развивать навыки принятия решений, командной работы, коммуникации и лидерства.</div>
+    <source srcset="https://mgubs.ru/wp-content/uploads/2023/07/bg-ma-991.jpg.webp" type="image/webp" media="(max-width: 991px)">
+    <source srcset="https://mgubs.ru/wp-content/uploads/2023/07/bg-ma-991.jpg" type="image/jpeg" media="(max-width: 991px)">
+
+    <source srcset="https://mgubs.ru/wp-content/uploads/2023/07/bg-ma-1920.jpg.webp" type="image/webp">
+    <img decoding="async" class="image-bg" src="https://mgubs.ru/wp-content/uploads/2023/07/bg-ma-1920.jpg" data-src="https://mgubs.ru/wp-content/uploads/2023/07/bg-ma-1920.jpg" alt="MGU">
+  </picture>
+  <div class="container df-sp-fs">
+    <div class="content-title">
+      <h1 class="h1">Магистерские программы</h1>
     </div>
-    <div class="advantages-item">
-      <img class="advantages-item__icon" src="<?= $url; ?>/assets/img/mgu-advantages/icon-2.svg" alt="alto" />
-      <div class="advantages-item__content"><strong>Learning by doing</strong> <br/>Образовательный процесс построен на использовании активных методов обучения. Данная методика позволяет студентам развивать навыки принятия решений, командной работы, коммуникации и лидерства.</div>
-    </div>
-    <div class="advantages-item">
-      <img class="advantages-item__icon" src="<?= $url; ?>/assets/img/mgu-advantages/icon-3.svg" alt="alto" />
-      <div class="advantages-item__content"><strong>Learning by doing</strong> <br/>Образовательный процесс построен на использовании активных методов обучения. Данная методика позволяет студентам развивать навыки принятия решений, командной работы, коммуникации и лидерства.</div>
-    </div>
-    <div class="advantages-item">
-      <img class="advantages-item__icon" src="<?= $url; ?>/assets/img/mgu-advantages/icon-4.svg" alt="alto" />
-      <div class="advantages-item__content"><strong>Learning by doing</strong> <br/>Образовательный процесс построен на использовании активных методов обучения. Данная методика позволяет студентам развивать навыки принятия решений, командной работы, коммуникации и лидерства.</div>
-    </div>
-    <div class="advantages-item">
-      <img class="advantages-item__icon" src="<?= $url; ?>/assets/img/mgu-advantages/icon-5.svg" alt="alto" />
-      <div class="advantages-item__content"><strong>Learning by doing</strong> <br/>Образовательный процесс построен на использовании активных методов обучения. Данная методика позволяет студентам развивать навыки принятия решений, командной работы, коммуникации и лидерства.</div>
-    </div>
-    <div class="advantages-item">
-      <img class="advantages-item__icon" src="<?= $url; ?>/assets/img/mgu-advantages/icon-6.svg" alt="alto" />
-      <div class="advantages-item__content"><strong>Learning by doing</strong> <br/>Образовательный процесс построен на использовании активных методов обучения. Данная методика позволяет студентам развивать навыки принятия решений, командной работы, коммуникации и лидерства.</div>
+    <div class="content-descr with-border">
+      <h2 class="h2">Стратегия бизнеса <br/><br/>Управление бизнесом и предпринимательство</h2>
+      <div class="descr">Специализированные учебные программы, разработанные для выпускников ВУЗов, имеющих степень бакалавра или диплом специалиста, нацеленных на построение карьеры или развитие собственного бизнеса, мотивированных к серьезной учебе</div>
     </div>
   </div>
-</section>
+</div>
+
 
 <?php // echo do_shortcode('[contact-form-7 id="d65f8de" title="Контактная форма 1"]'); ?>
 <?php // echo do_shortcode('[featured_posts]'); ?>
