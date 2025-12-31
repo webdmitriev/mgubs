ff7abcc3d10f6c5de3e46e71c4e364d476990d86
webdmitriev <webdmitriev@gmail.com>
2026-01-01 00:59:24 +0300

    block-30 is ready (front)

--- pages/page-development.php
+++ pages/page-development.php
@@ -12,8 +12,32 @@ the_content();
 ?>
 
 <div class="block-standard block-30">
-  <div class="container">
-    www
+  <div class="container"><h2 class="h2">Title</h2></div>
+  <div class="container df-sp-fs">
+    <div class="content-descr">
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+      <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, fugiat.</div>
+    </div>
+    <div class="content-images">
+      <div class="content-author">
+        <img decoding="async" src="https://mgubs.ru/wp-content/uploads/2023/05/different.jpg" alt="MGU" />
+        <div class="content-author__data">
+          <h3 class="h3">Виханский Олег Самуилович</h3>
+          <div class="descr">Декан Высшей школы бизнеса МГУ. <br/>Профессор, доктор экономических наук. <br/>Заслуженный профессор Московского университета. <br/>Заведующий кафедрой менеджмента ВШБ МГУ.</div>
+        </div>
+      </div>
+      <img decoding="async" src="https://mgubs.ru/wp-content/uploads/2024/01/cube-all-1.svg" alt="MGU" />
+    </div>
   </div>
 </div>
 
